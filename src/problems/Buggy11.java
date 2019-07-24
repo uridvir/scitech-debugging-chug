058a21528d6d28f342f38005c3b3e04d4fc7ec77
uridvir <uri.j.dvir@gmail.com>
2019-07-24 02:10:37 -0400

    Corrected spelling of Fibonacci in Buggy11.java

--- src/problems/Buggy11.java
+++ src/problems/Buggy11.java
@@ -4,7 +4,7 @@ import library.BuggyCode;
 
 public class Buggy11 extends BuggyCode<Integer, Integer> {
 
-	// Returns the n-th Fibonaci number (starting at zero) using RECURSION!
+	// Returns the n-th Fibonacci number (starting at zero) using RECURSION!
 	public Integer run(Integer n) {
 		
 		return run(n - 1) + run(n - 2);
